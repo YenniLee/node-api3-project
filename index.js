c8791792bdfc19f49206a66871376fbf65b3e9af
Jennie Van <missjenvan@gmail.com>
2019-12-05 11:54:37 -0800

    dyanmic port

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
+require('dotenv').config();
+
 const server = require('./server.js');
-const port = 4000;
+const port = process.env.PORT || 4000;
 
 server.listen(port, () => {
     console.log(`*** server listening on port ${port} ***`);
